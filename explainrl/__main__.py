fae51a7ffca2c085ba403c9803683d3cda79bfd2
KanishAnand <therealkanish@gmail.com>
2021-02-23 23:19:57 +0530

    baseline code

--- explainrl/__main__.py
+++ explainrl/__main__.py
@@ -1,11 +1,27 @@
+import os
 import argparse
+import pandas as pd
 
-from explainrl.environment.display import GridRender
+from environment.state import GridState
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input-file', required=True, help='Input File Path')
+    parser.add_argument('--dataset-path', required=True,
+                        help='Path for folder containing dataset')
     args = parser.parse_args()
 
-    GridRender.load(args.input_file)
+    for file in sorted(os.listdir(args.dataset_path)):
+        path = os.path.join(args.dataset_path, file)
+        obj = GridState(path)
+        moves = "ULULDRURDL"
+        for direction in moves:
+            obj.move(direction)
+            print(obj.tiles, obj.targets)
+            if obj.tiles == obj.targets:
+                break
+                print("Puzzle Solved")
+        break
+    #     df = df.append({'filename': file, 'moves': moves}, ignore_index = False)
+    # df = pd.DataFrame(columns={'filename', 'moves'})
+    # print(df.head())
