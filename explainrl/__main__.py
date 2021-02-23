4c9537cc6c1475e6c24f3a37585cf6f231a06c04
KanishAnand <therealkanish@gmail.com>
2021-02-24 00:39:46 +0530

    generator updated

--- explainrl/__main__.py
+++ explainrl/__main__.py
@@ -3,18 +3,16 @@ import os
 import argparse
 
 from data_generator import generator
-# from explainrl.environment.display import GridRender
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--output-path', required=True,
                         help='Output Data Path')
-    parser.add_argument('--count', type = int, required=True,
+    parser.add_argument('--count', type=int, required=True,
                         help='Number of test files')
     args = parser.parse_args()
 
-    for i in tqdm.tqdm(range(1,args.count+1)):
+    for i in tqdm.tqdm(range(1, args.count+1)):
         path = os.path.join(args.output_path, str(i).zfill(4) + '.txt')
         generator.generate(path)
-
--- explainrl/data_generator/generator.py
+++ explainrl/data_generator/generator.py
@@ -1,18 +1,21 @@
 import random
+from collections import defaultdict
 from data_generator import config
 import numpy as np
 
 random.seed(config.RANDOM_SEED)
 
+
 def move_value(move):
     if(move == "U"):
-        return (-1,0)
+        return (-1, 0)
     elif(move == "D"):
-        return (1,0)
+        return (1, 0)
     elif(move == "L"):
-        return (0,-1)
+        return (0, -1)
     elif(move == "R"):
-        return (0,1)
+        return (0, 1)
+
 
 def make_move(N, M, move, tiles, grid):
     delta_r, delta_c = move_value(move)
@@ -25,14 +28,25 @@ def make_move(N, M, move, tiles, grid):
         tiles[idx] = tile
     return flag, tiles
 
+
+def random_moves(no_moves):
+    seq = ['U', 'D', 'L', 'R']
+    moves = random.choice(seq)
+    for i in range(1, no_moves):
+        mv = random.choice(seq)
+        while moves[-1] == mv:
+            mv = random.choice(seq)
+        moves += mv
+    return moves
+
+
 def generate(output_file):
     fout = open(output_file, "w")
-
-    N = random.randint(config.N_MIN, config.N_MAX)
-    M = random.randint(config.M_MIN, config.M_MAX)
+    N = random.choices(config.N_CHOICES, weights=config.N_WEIGHTS)[0]
+    M = random.choices(config.M_CHOICES, weights=config.M_WEIGHTS)[0]
     fout.write("{} {}\n".format(N, M))
 
-    PROB = random.choices(config.PROB_CHOICES, weights = config.PROB_WEIGHTS)[0]
+    PROB = random.choices(config.PROB_CHOICES, weights=config.PROB_WEIGHTS)[0]
     arr = np.full(((N, M)), ['.'], dtype=str)
     for i in range(N):
         for j in range(M):
@@ -41,7 +55,8 @@ def generate(output_file):
 
     np.savetxt(fout, arr, fmt='%s', delimiter='')
 
-    no_sources = random.choices(config.SOURCES_CHOICES, weights = config.SOURCES_WEIGHTS)[0]
+    no_sources = random.choices(
+        config.SOURCES_CHOICES, weights=config.SOURCES_WEIGHTS)[0]
     sources = set()  # set taken so that there are no two sources at same coordinates
     while(len(sources) != no_sources):
         row = random.randint(0, N-1)
@@ -53,17 +68,19 @@ def generate(output_file):
 
     no_moves = random.randint(config.MOVES_MIN, config.MOVES_MAX)
 
-    seq = ['U', 'D', 'L', 'R']
-    moves = ''.join(random.choices(
-        seq, weights=None, cum_weights=None, k=no_moves))
+    moves = random_moves(no_moves)
 
     destinations = list(sources)
-    for move in moves:
+    dict = defaultdict(int)
+    for cnt, move in enumerate(moves):
         for _ in range(max(N, M) + 1):
             flag, destinations = make_move(N, M, move, destinations, arr)
             if flag == 0:
                 break
+        if cnt >= config.MOVES_THRESHOLD:
+            dict[tuple(destinations)] += 1
 
+    destinations = min(dict, key=dict.get)
     for src, dest in zip(sources, destinations):
         fout.write("{} {} {} {}\n".format(
             src[0], src[1], dest[0], dest[1]))
